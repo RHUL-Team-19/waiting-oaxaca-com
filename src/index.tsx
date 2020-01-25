c2d0f6681ac2a479e8b915ef3c844d87b1d693b0
izexi <43889168+izexi@users.noreply.github.com>
2020-01-25 11:48:12 +0000

    feat: redirect to /login on load

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Login from './Login';
 import * as serviceWorker from './serviceWorker';
+import { createBrowserHistory } from 'history';
+import { Router, Route, Redirect } from 'react-router-dom';
 
-// TODO: route to /login on load
-ReactDOM.render(<Login />, document.getElementById('root'));
+const history = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={history}>
+    <Redirect to="/login" />
+    <Route exact path="/login">
+      <Login />
+    </Route>
+  </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
