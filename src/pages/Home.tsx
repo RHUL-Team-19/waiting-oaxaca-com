af263d1f16e8ce79cdf5431541c631382a3e2fcd
izexi <43889168+izexi@users.noreply.github.com>
2020-01-28 19:35:08 +0000

    feat(Home): add navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import history from '../history';
 import { Router } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import '../style/style.css';
 
 export default class Home extends React.Component {
   render() {
     return (
-      <Router history={history}>{/* TODO: Create a dashboard/navbar */}</Router>
+      <Router history={history}>
+        <Navbar />
+      </Router>
     );
   }
 }
