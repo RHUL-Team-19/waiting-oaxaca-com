35596613c46b9d9a18ebc96facdc85bfedcdfe13
izexi <43889168+izexi@users.noreply.github.com>
2020-01-28 22:17:34 +0000

    src(components): create NavOption

--- src/components/NavOption.tsx
+++ src/components/NavOption.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+import history from '../history';
+import NavItem from './NavItem';
+import { capitalise } from '../Util';
 
 const NavOption = ({ option }: { option: string }) => (
-  <div className="navbar-item has-dropdown is-hoverable">
-    <a className="navbar-link">{option}</a>
-    <div className="navbar-dropdown">
-      <a className="navbar-item">Find</a>
-      <a className="navbar-item">View all</a>
-      <hr className="navbar-divider" />
-      <a className="navbar-item">Create</a>
+  <Router history={history}>
+    <div className="navbar-item has-dropdown is-hoverable">
+      <a className="navbar-link">{capitalise(option)}</a>
+      <div className="navbar-dropdown">
+        <NavItem selected={option} option="find" />
+        <NavItem selected={option} option="view" />
+        <hr className="navbar-divider" />
+        <NavItem selected={option} option="create" />
+      </div>
     </div>
-  </div>
+  </Router>
 );
 
 export default NavOption;
