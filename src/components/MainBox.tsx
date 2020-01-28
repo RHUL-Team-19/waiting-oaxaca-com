ad9a3ddaadf8172ae905834e05f1657dae038528
izexi <43889168+izexi@users.noreply.github.com>
2020-01-28 21:43:36 +0000

    src(components): create MainBox

---
+++ src/components/MainBox.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledBox = styled.div`
+  margin-top: 30px;
+  margin-left: auto;
+  width: 95%;
+  margin-right: auto;
+  height: 836px;
+`;
+
+const MainBox = () => <StyledBox className="box" />;
+
+export default MainBox;
