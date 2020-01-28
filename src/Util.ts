3ba8b61196f3c7ce9ecb0373926ed4af330978bf
izexi <43889168+izexi@users.noreply.github.com>
2020-01-28 23:09:25 +0000

    fix(Util.redirect): use base url for nested routes

--- src/Util.ts
+++ src/Util.ts
@@ -3,4 +3,5 @@ import history from './history';
 export const capitalise = (word: string) =>
   word.replace(/./, firstLetter => firstLetter.toUpperCase());
 
-export const redirect = (path: string) => history.push(`./${path}`);
+export const redirect = (path: string) =>
+  history.push(`${process.env.PUBLIC_URL}/${path}`);
