de3e0775e7f98e93b7b27e571278676350f29498
izexi <43889168+izexi@users.noreply.github.com>
2020-01-28 21:44:05 +0000

    feat(Home): add MainBox to hold other elements

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,14 @@ import history from '../history';
 import { Router } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import '../style/style.css';
+import MainBox from '../components/MainBox';
 
 export default class Home extends React.Component {
   render() {
     return (
       <Router history={history}>
         <Navbar />
+        <MainBox />
       </Router>
     );
   }
