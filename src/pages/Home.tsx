58056f4dec48de1eceedc595df29f510967d5602
Winkie <59916015+zfac170@users.noreply.github.com>
2020-01-29 12:18:22 +0000

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,27 @@ import { Router } from 'react-router-dom';
 export default class Home extends React.Component {
   render() {
     return (
-      <Router history={history}>{/* TODO: Create a dashboard/navbar */}</Router>
+      <Router history={history}>{
+        <mdb-navbar SideClass="navbar navbar-expand-lg navbar-black">
+          <links>
+              <a class="navbar-brand" href="#">Restaurant name</a>
+              <ul class="navbar-nav mr-auto mt-lg-0">
+                  <li class="nav-item active">
+                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="#">Menu</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="#">Order</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="#">Payment</a>
+                  </li>
+              </ul>
+          </links>
+         </mdb-navbar>
+          /*Create a dashboard/navbar */}</Router>
     );
   }
 }
