b79a895ab2915d8ece46efc172bd0acb771ed3e3
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-02-11 10:29:05 +0000

    src: delete duplicated Util

--- src/Util.ts
+++
@@ -1,6 +0,0 @@
-import history from './history';
-
-export const capitalise = (word: string) =>
-  word.replace(/./, firstLetter => firstLetter.toUpperCase());
-
-export const redirect = (path: string) => history.push(`./${path}`);
